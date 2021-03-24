c85a1aa156fb5f525250e6e2f06b7877b7d8a20a
SunittaT <sunitta.thomas@outlook.com>
2021-03-24 13:34:46 +0530

    added the User profile, comments ,updated coupons

--- amazed/config.js
+++ amazed/config.js
@@ -1,7 +1,3 @@
-// module.exports= {
-//     'secret': 'loginsecret'
-// }
-
 const config = {
     'secret': 'loginsecret',
      google: {
@@ -11,7 +7,10 @@ const config = {
      facebook:{
       FACEBOOK_APP_ID:'1339678449764671',
       FACEBOOK_APP_SECRET:'eb3b5bced032f18576c194e4425d8970'
-
-   }
+      },
+      auth:{
+         user:'customerserviceamazed@gmail.com',
+         pass:'Am@zed123'
+      }
 }
 export default config;
